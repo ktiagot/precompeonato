920c8b27d9a481a48d5015942759622c9b4f6050
Tiagones <tiagocalutis@hotmail.com.br>
2026-02-28 00:54:19 -0300

    Atualizando informações do banco

--- db.js
+++ db.js
@@ -8,14 +8,15 @@ console.log('  - Database:', process.env.DB_NAME);
 console.log('  - Port:', process.env.DB_PORT);
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
+    host: process.env.DB_HOST === 'localhost' ? '127.0.0.1' : process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT,
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+    socketPath: undefined // Força TCP/IP ao invés de socket Unix
 });
 
 // Log de erros global
